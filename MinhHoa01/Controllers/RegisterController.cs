0d5dd087e7d25ab29dfec93313f7d493b943d75b
ADMIN <ADMIN@PC302>
2025-05-12 08:58:55 +0700

    Add project files.

---
+++ MinhHoa01/Controllers/RegisterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MinhHoa01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+public class RegisterController : Controller
+{
+    private IHostingEnvironment hosting;
+    public RegisterController(IHostingEnvironment _hosting)
+    {
+        hosting = _hosting;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult XuLy(PersonViewModel m, IFormFile FHinh)
+    {
+        if (FHinh != null)
+        {
+            //xử lý upload
+            // string filename = FHinh.FileName;
+            string filename = Guid.NewGuid().ToString() + Path.GetExtension(FHinh.FileName);
+            string path = Path.Combine(hosting.WebRootPath, "images");
+            using (var filestream = new FileStream(Path.Combine(path, filename), FileMode.Create))
+            {
+                //sao chép lên server
+                FHinh.CopyTo(filestream);
+            }
+            m.Picture = filename;
+        }
+        return View(m);
+    }
+}
---
+++ MinhHoa01/Models/PersonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhHoa01.Models
+{
+    public class PersonViewModel
+    {
+        public string FullName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Picture { get; set; }
+    }
+}
